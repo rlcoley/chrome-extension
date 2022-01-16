ba9cbb0d8cb5a15f55b2340f2a2f212e1ac53c69
Ross Coley <rosscoley1002@gmail.com>
2022-01-15 22:36:37 -0500

    added keypress eventListener on input

--- chrome extension-js/index.js
+++ chrome extension-js/index.js
@@ -4,11 +4,23 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+inputEl.addEventListener('keypress', pressedEnter)
+
+function pressedEnter(pressed) {
+    if (pressed.which === 13){
+        console.log("Key Pressed")
+        // console.log(myLeads)
+
+        myLeads.push(inputEl.value)
+        console.log(myLeads)
 
 
-    
-      
+        // 2. Call the renderLeads() function
+        renderLeads()
+        inputEl.value =" " 
 
+    }
+}
 
 
 inputBtn.addEventListener("click", function() {
