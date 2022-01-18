49468e5ed7c7ad50705a10c263f27e59b36b2f92
Ross Coley <rosscoley1002@gmail.com>
2022-01-17 20:37:11 -0500

    added delete button functionality
    made delete-btn variable
    
    added click eventlistener to delete last item in array in localStoreage and update the DOM to show updated content

--- chrome extension-js/index.js
+++ chrome extension-js/index.js
@@ -1,14 +1,71 @@
 
 let myLeads = []
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
+
+
+// Get the leads from the localStorage - PS: JSON.parse()
+// Store it in a variable, leadsFromLocalStorage
+let leadsFromLocalStorage =  JSON.parse(localStorage.getItem("myLeads"))
+
+
+// Check if leadsFromLocalStorage is truthy
+// If so, set myLeads to its value and call renderLeads()
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+deleteBtn.addEventListener('click', function(){
+//  when click delete last item in localStorage and update the DOM
+    console.log("swac");
+    let deleteLastOne = leadsFromLocalStorage[leadsFromLocalStorage.length - 1]
+    console.log(deleteLastOne);
+    leadsFromLocalStorage.pop(deleteLastOne)
+    renderLeads()
+    console.log(leadsFromLocalStorage)
+
+    
+})
+
+
+
+
+// deleteBtn.addEventListener('dblclick', function(){
+    
+//     // When clicked, clear localStorage, myLeads, and the DOM    
+        
+//         localStorage.clear()
+//         myLeads= []
+//         ulEl.innerHTML = " "
+//         console.log("ya bish")
+// })
+
+
+// let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+// Log out the variable
+// localStorage.clear()
+console.log(leadsFromLocalStorage)
 
 
 function getValue() {
     myLeads.push(inputEl.value)
     inputEl.value =" " 
+
+     // Save the myLeads array to localStorage 
+    // PS: remember JSON.stringify()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )  
+    renderLeads()
+    
+    // To verify that it works:
+    console.log( localStorage.getItem("myLeads") )
+
     renderLeads()
     
 }
@@ -24,7 +81,7 @@ function pressedEnter(pressed) {
         // console.log(myLeads)
 
 
-        // 2. Call the renderLeads() function
+        // Call the renderLeads() function
 
     }
 }
@@ -44,9 +101,17 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems +=  `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>
+    `
+
     }
     ulEl.innerHTML = listItems
+    // console.log(listItems);
 }
 
 
